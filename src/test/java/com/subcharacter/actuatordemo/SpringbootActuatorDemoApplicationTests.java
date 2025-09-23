5d991a0c2caa98512cac22f3c8b238acd8c148cc
subCharacters <wldns59@naver.com>
2025-09-24 07:36:57 +0900

    initialize commit

---
+++ src/test/java/com/subcharacter/actuatordemo/SpringbootActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subcharacter.actuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActuatorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
